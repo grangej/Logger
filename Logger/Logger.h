58346ee0cdf0c129623c1509064216ebd210a448
John Grange <john.grange@ymedialabs.com>
2015-01-01 20:37:33 -0800

    Initial Commit

---
+++ Logger/Logger.h
@@ -0,0 +1,19 @@
+//
+//  Logger.h
+//  Logger
+//
+//  Created by John Grange on 1/1/15.
+//  Copyright (c) 2015 Y Media Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logger/PublicHeader.h>
+
+
